0a33695d4d2a810f846fc991cfcb1d9e38ef8d1d
남용호 <nam-yongho@nam-yonghoui-MacBookPro.local>
2021-02-10 17:18:17 +0900

    C++

---
+++ C++/Cshap/forcshp.cs
@@ -0,0 +1,12 @@
+using System;
+namespace fortest
+{
+    
+    class Fi
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("asdfaadf");
+        }
+    }
+}
---
+++ C++/algorithms.cpp
@@ -0,0 +1,36 @@
+
+#include<iostream>
+#include<list>
+using namespace std;
+int *checking(){
+    int arr[2];
+    return arr;
+}
+int main(int argc, char const *argv[])
+{
+   /* int N = 5;
+    int blocks[15][2];
+    int index = 0;
+    int x = 0;
+    int y = 0;
+    int board[5][6] = {
+        {3,2,5,1,3,2},
+        {4,1,4,2,3,2},
+        {3,3,1,1,2,4},
+        {5,5,1,3,5,4},
+        {4,5,2,4,1,5},
+    };
+    for(int i = 1;i <= N;++i){
+        for(int t = N;t >= i;--t){
+            blocks[index][0] = i;
+            blocks[index][1] = t;
+            ++index;
+        }
+    }
+    int full = 0;
+    while(full){
+        
+    }*/
+    cout<<"adf"<<endl;
+    return 0;
+}
